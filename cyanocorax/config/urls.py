1f05a8ca7debdccc7f4ef2baec064b779881c5fa
Miguel Suescun <debian789@gmail.com>
2015-12-11 17:07:13 -0500

    cambio de url para obtener los datos de json de productos

--- cyanocorax/config/urls.py
+++ cyanocorax/config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^login/$',  'webclient.views.ingreso'),
     url(r'^logout/$', 'django.contrib.auth.views.logout',  {'next_page': '/'}),
     url(r'^productos/explorar$', 'productos.views.explorar_productos'),
-    url(r'^productos/json$', 'productos.views.explorar_productos_json'),
+    url(r'^productos/json.js$', 'productos.views.explorar_productos_json'),
 ]
